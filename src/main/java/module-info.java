3e8fe8e09e84be0f3e4f004f7588c99f1c8f2b67
Joseph Barr <jbarr89@yahoo.com>
2021-03-22 08:40:22 -0500

    Added neccessary parameters to module-info.java.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module JAAB.Apps {
     requires javafx.controls;
     requires javafx.fxml;
     requires poi.ooxml;
+    requires javafx.graphics;
 
     opens JAAB.Apps to javafx.fxml;
     exports JAAB.Apps;
